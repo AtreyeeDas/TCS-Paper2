8074f8391bdf6ab258d1d9ba9b7b4af96c6f2995
Atreyee Das <148999496+AtreyeeDas@users.noreply.github.com>
2026-08-14 10:23:33 +0530

    Create conda_inst2.py

---
+++ code/phase2/src/conda_inst2.py
@@ -0,0 +1,5 @@
+# Activate your existing Conda environment
+conda activate asil_env
+
+# Install/verify HuggingFace ecosystem, audio loaders, and acceleration packages
+pip install transformers accelerate sentencepiece soundfile librosa pandas tqdm
